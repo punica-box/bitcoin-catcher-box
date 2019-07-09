876e529edb8787a3c066ae3b5aa7561e2eb8585c
dingxin <wdx7266@vip.qq.com>
2019-07-09 16:46:49 +0800

    update contract

--- contracts/bitcoin_catcher.py
+++ contracts/bitcoin_catcher.py
@@ -14,16 +14,17 @@ USER_KEY = 'user'
 SCORE_KEY = 'score'
 
 registerEvent = RegisterAction("register", "user_address", "user_name")
-updateScoreEvent = RegisterAction(
-    "updateScore", 'user_address', 'user_name', 'score')
+updateScoreEvent = RegisterAction("updateScore", 'user_address', 'user_name', 'score')
 
 
 def main(operation, args):
     if operation == 'register':
         return register(args[0], args[1])
-    elif operation == 'update_score':
+    if operation == 'get_user_name':
+        return get_user_name(args[0])
+    if operation == 'update_score':
         return update_score(args[0], args[1])
-    elif operation == 'get_score':
+    if operation == 'get_score':
         return get_score(args[0])
     return False
 
@@ -32,20 +33,26 @@ def register(user_address, user_name):
     if Get(ctx, concat(user_name, USER_KEY)):
         raise Exception("username exist")
     assert (CheckWitness(user_address))
-    Put(ctx, concat(user_name, USER_KEY), user_address)
+    Put(ctx, concat(user_address, USER_KEY), user_name)
     registerEvent(user_address, user_name)
     return True
 
+def get_user_name(user_address):
+    user_name = Get(ctx, concat(user_address, USER_KEY))
+    if not user_name:
+        raise Exception('username not exist')
+    return user_name
 
-def update_score(user_name, score):
-    user_address = Get(ctx, concat(user_name, USER_KEY))
-    if not user_address:
+
+def update_score(user_address, score):
+    user_name = Get(ctx, concat(user_address, USER_KEY))
+    if not user_name:
         raise Exception("username not exist")
     assert (CheckWitness(user_address))
-    Put(ctx, concat(user_name, SCORE_KEY), score)
+    Put(ctx, concat(user_address, SCORE_KEY), score)
     updateScoreEvent(user_address, user_name, score)
     return True
 
 
-def get_score(user_name):
-    return Get(ctx, concat(user_name, SCORE_KEY))
+def get_score(user_address):
+    return Get(ctx, concat(user_address, SCORE_KEY))
