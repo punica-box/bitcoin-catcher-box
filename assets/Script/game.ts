8bf650f09c297fcf0571a9fb64bc1ca7cd08cbbf
dingxin <wdx7266@vip.qq.com>
2019-07-09 15:11:48 +0800

    update usage

--- assets/Script/game.ts
+++ assets/Script/game.ts
@@ -1,13 +1,3 @@
-// Learn TypeScript:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
 const { ccclass, property } = cc._decorator;
 
 import { client } from "ontology-dapi";
@@ -42,7 +32,9 @@ export default class Game extends cc.Component {
     @property(cc.Float)
     showDuration: number = 0;
 
-    @property(cc.AudioClip)
+    @property({
+        type: cc.AudioClip
+    })
     scoreAudio: cc.AudioClip = null;
 
     // LIFE-CYCLE CALLBACKS:
