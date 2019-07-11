83f4f1a3c195ab0a12386302b17f555e10139643
dingxin <wdx7266@vip.qq.com>
2019-07-11 19:46:20 +0800

    add contract interface

--- assets/Script/index.ts
+++ assets/Script/index.ts
@@ -11,37 +11,51 @@ export default class NewClass extends cc.Component {
     @property(cc.Button)
     scoreButton: cc.Button = null;
 
-    private contractAddress: string = "a6be9cfd13c40be773c4dac2b9ca3c258c698214";
+    private contractAddress: string = "c389de61bd436a68945581b1e2d2eec395ce4d8e";
 
     // LIFE-CYCLE CALLBACKS:
 
     async onLoad() {
         await client.registerClient({});
-        await this.getUserName();
-
-
         this.playButton.node.on('click', this.startScene, this);
         this.scoreButton.node.on('click', this.startScene, this);
     }
+
+    async register() {
+        let accountAddress: String = await client.api.asset.getAccount();
+        let txHash = await client.api.smartContract.invoke({
+            scriptHash: this.contractAddress,
+            operation: "register",
+            args: [{ type: "String", value: accountAddress }, { type: 'String', value: "NashMiao" }]
+        });
+        console.log(txHash);
+    }
+
     async getUserName() {
-        console.log("here");
+        let accountAddress = await client.api.asset.getAccount();
+        console.log(accountAddress);
         let result = await client.api.smartContract.invokeRead({
             scriptHash: this.contractAddress,
             operation: 'get_user_name',
-            args: []
+            args: [{ type: 'String', value: accountAddress }]
         });
+        console.log(result);
         if (!result) {
-            console.log("");
             return "";
         }
         console.log(result);
     }
 
-    startScene(event) {
+    async startScene(event) {
         console.log(event);
         switch (event.node.name) {
             case 'playButton':
-                cc.director.loadScene('BitcoinCatcher');
+                let userName = await this.getUserName();
+                if (userName.length !== 0) {
+                    cc.director.loadScene('BitcoinCatcher');
+                    break;
+                }
+                await this.register();
                 break;
             case 'scoreButton':
                 break;
