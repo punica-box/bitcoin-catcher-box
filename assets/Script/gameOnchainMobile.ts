907261bf18322026fc020f42d2d9da93d56f4d9c
dingxin <wdx7266@vip.qq.com>
2019-07-18 00:01:43 +0800

    support mobile

---
+++ assets/Script/gameOnchainMobile.ts
@@ -0,0 +1,148 @@
+const { ccclass, property } = cc._decorator;
+
+import { client } from 'cyanobridge';
+
+@ccclass
+export default class GameOnchain extends cc.Component {
+
+    @property(cc.Node)
+    ontologyer: cc.Node = null;
+
+    @property(cc.Prefab)
+    bitCoinPreFab: cc.Prefab = null;
+
+    @property(cc.Float)
+    maxCoinShowTime: number = 0;
+
+    @property(cc.Float)
+    minCoinShowTime: number = 0;
+
+    @property(cc.Float)
+    groundY: number = 0;
+
+    @property(cc.Float)
+    private score: number = 0;
+
+    @property(cc.Label)
+    scoreLabel: cc.Label = null;
+
+    @property(cc.Float)
+    timer: number = 0;
+
+    @property(cc.Float)
+    showDuration: number = 0;
+
+    @property({
+        type: cc.AudioClip
+    })
+    scoreAudio: cc.AudioClip = null;
+
+    private gasPrice = 500;
+
+    private gasLimit = 20000;
+
+    private contractAddress: string = "6fa5b9c9bd8ae8e74773d910619622625e36d4d8";
+
+    // LIFE-CYCLE CALLBACKS:
+
+    async onLoad() {
+        this.score = 0;
+        this.timer = 0;
+        try {
+            await client.registerClient();
+        }
+        catch (e) {
+            console.log(e);
+        }
+        this.newBitCoin();
+    }
+
+    start() {
+
+    }
+
+    newBitCoin() {
+        let newCoin = cc.instantiate(this.bitCoinPreFab);
+        this.node.addChild(newCoin);
+        newCoin.setPosition(this.getNewCoinPostion());
+        newCoin.getComponent('bitcoin').game = this;
+        this.showDuration = this.minCoinShowTime + Math.random() * (this.maxCoinShowTime - this.minCoinShowTime);
+        this.timer = 0;
+    }
+
+    getNewCoinPostion() {
+        var randX = 0;
+        var randY = this.groundY + Math.random() * this.ontologyer.getComponent('ontologyer').jumpHeight + 50;
+        var maxX = this.node.width / 2;
+        randX = (Math.random() - 0.5) * 2 * maxX;
+        return cc.v2(randX, randY);
+    }
+
+    async getAcountAddress() {
+        let response = await client.api.asset.getAccount({
+            dappName: "Bitcoin Catcher",
+            dappIcon: ""
+        });
+        let address = "";
+        if (response["error"] === 0) {
+            address = response['result'];
+        }
+        return address;
+    }
+
+    async update(dt) {
+        if (this.timer > this.showDuration) {
+            await Alert.show("Score: " + this.score, async f => {
+                let accountAddress = await this.getAcountAddress();
+                try {
+                    let response = await client.api.smartContract.invokePasswordFree({
+                        scriptHash: this.contractAddress,
+                        operation: "update_score",
+                        args: [{ type: "String", value: accountAddress }, { type: 'Integer', value: this.score }],
+                        gasPrice: this.gasPrice,
+                        gasLimit: this.gasLimit,
+                        payer: accountAddress,
+                        config: {
+                            login: false,
+                            message: "",
+                            url: ""
+                        }
+                    });
+                    if (response["error"] === 0) {
+                        let txHash = response['transaction'];
+                        Alert.show("TxHash:" + txHash, function () {
+                            cc.director.loadScene("BitcoinCatcherOnchainMobile");
+                        }, function () {
+                            cc.director.loadScene("IndexMobile");
+                        });
+                    } else {
+                        cc.director.loadScene("IndexMobile");
+                    }
+                } catch (e) {
+                    console.log(e);
+                    cc.director.loadScene("IndexMobile");
+                }
+            }, this.loadIndex);
+            this.ontologyer.stopAllActions();
+            this.ontologyer.active = false;
+            try {
+                this.node.getChildByName("bitcoin").active = false;
+            } catch (e) {
+                console.log(e);
+            }
+            this.enabled = false;
+            return;
+        }
+        this.timer += dt;
+    }
+
+    async loadIndex() {
+        cc.director.loadScene("Index");
+    }
+
+    gainScore() {
+        this.score += 1;
+        this.scoreLabel.string = 'Score: ' + this.score;
+        cc.audioEngine.playEffect(this.scoreAudio, false);
+    }
+}
---
+++ assets/Script/indexMobile.ts
@@ -0,0 +1,155 @@
+const { ccclass, property } = cc._decorator;
+
+import { client } from "cyanobridge";
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Button)
+    playButton: cc.Button = null;
+
+    @property(cc.Button)
+    scoreButton: cc.Button = null;
+
+    private gasPrice = 500;
+
+    private gasLimit = 20000;
+
+    private walletExist = false;
+
+    private contractAddress: string = "6fa5b9c9bd8ae8e74773d910619622625e36d4d8";
+
+    // LIFE-CYCLE CALLBACKS:
+
+    async onLoad() {
+        try {
+            await client.registerClient();
+            this.walletExist = true;
+        }
+        catch (e) {
+            await Alert.show("Please open it in ONTO.", function () {
+                cc.sys.openURL("https://onto.app/");
+            });
+        }
+        this.playButton.node.on('click', this.startScene, this);
+        this.scoreButton.node.on('click', this.startScene, this);
+    }
+
+    async startScene(event) {
+        console.log(event);
+        switch (event.node.name) {
+            case 'playButton':
+                if (this.walletExist === false) {
+                    await Alert.show("Do you want to play it without blockchain?", function () {
+                        cc.director.loadScene('BitcoinCatcher');
+                    });
+                    break;
+                }
+                let userName = await this.getUserName();
+                console.log("userName: " + userName);
+                if (userName.length === 0) {
+                    await this.register();
+                    break;
+                }
+                await Alert.show("Welcome " + userName, function () {
+                    cc.director.loadScene('BitcoinCatcherOnchainMobile');
+                });
+                break;
+            case 'scoreButton':
+                if (this.walletExist === false) {
+                    await Alert.show("Please install ONTO first", function () {
+                        cc.sys.openURL("https://onto.app/");
+                    });
+                    break;
+                }
+                let score = await this.getScore();
+                if (score !== "") {
+                    await Alert.show("score: " + score, null, null, false);
+                }
+                break;
+            default:
+                break
+        }
+    }
+
+    async getAcountAddress() {
+        let response = await client.api.asset.getAccount({
+            dappName: "Bitcoin Catcher",
+            dappIcon: ""
+        });
+        let address = "";
+        if (response["error"] === 0) {
+            address = response['result'];
+        }
+        return address;
+    }
+
+
+    async getUserName() {
+        let accountAddress = await this.getAcountAddress();
+        console.log(accountAddress);
+        try {
+            let response = await client.api.smartContract.invokeRead({
+                scriptHash: this.contractAddress,
+                operation: "get_user_name",
+                args: [{ type: 'String', value: accountAddress }],
+                gasPrice: this.gasPrice,
+                gasLimit: this.gasLimit
+            });
+            if (response["error"] !== 0) {
+                return "";
+            }
+            return response["result"]["Result"];
+        } catch (e) {
+            console.log(e);
+            return '';
+        }
+    }
+
+    async getScore() {
+        try {
+            let accountAddress = await this.getAcountAddress();
+            let response = await client.api.smartContract.invokeRead({
+                scriptHash: this.contractAddress,
+                operation: 'get_score',
+                args: [{ type: 'String', value: accountAddress }],
+                gasPrice: this.gasPrice,
+                gasLimit: this.gasLimit
+            });
+            if (response["error"] !== 0) {
+                await Alert.show(response["result"], null, null, false);
+                return "";
+            }
+            let score = response["result"]["Result"];
+            if (score === "") {
+                score = 0;
+            }
+            return score;
+        } catch (e) {
+            console.log(e);
+            return 0;
+        }
+    }
+
+    async register() {
+        try {
+            let accountAddress = await this.getAcountAddress();
+            let response = await client.api.smartContract.invoke({
+                scriptHash: this.contractAddress,
+                operation: "register",
+                args: [{ type: "String", value: accountAddress }, { type: 'String', value: "NashMiao" }],
+                gasPrice: this.gasPrice,
+                gasLimit: this.gasLimit,
+                payer: accountAddress
+            });
+            if (response["error"] === 0) {
+                let txHash = response['transaction'];
+                Alert.show("TxHash:" + txHash, function () {
+                    cc.director.loadScene("BitcoinCatcherOnchainMobile");
+                });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
